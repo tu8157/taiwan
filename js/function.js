09b2264ff7f9581f494fd0fe038cdb5eca453c33
tu8157 <tu8157642@gmail.com>
2024-10-06 01:31:12 +0900

    10/6

--- js/function.js
+++ js/function.js
@@ -7,3 +7,27 @@ $(function() {
 
 
 });
+
+
+
+window.addEventListener('load', function () {
+
+    function setupImageSlider(containerClass, imgClass, interval = 6000) {
+      const images = document.querySelectorAll(`.${containerClass} .${imgClass}`);
+      let number = 0;
+  
+      function changeImage() {
+        images[number].classList.remove('active');
+        number = (number + 1) % images.length;
+        images[number].classList.add('active');
+      }
+  
+      // スライドショーを開始
+      setInterval(changeImage, interval);
+    }
+  
+    // mv1とmv2でスライドショーを初期化
+    setupImageSlider('mv1', 'mv1__img');
+    setupImageSlider('mv2', 'mv2__img');
+  });
+  
