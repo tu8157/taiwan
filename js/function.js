a69a36db15dd10cfd7f73b72d29e7e3a70d64d9a
tu8157 <tu8157642@gmail.com>
2024-10-08 01:28:12 +0900

    10/7

--- js/function.js
+++ js/function.js
@@ -1,9 +1,5 @@
-$(function() {
-// ここにjQueryを記述
-
-
-
-
+$(function () {
+  // ここにjQueryを記述
 
 
 });
@@ -12,22 +8,34 @@ $(function() {
 
 window.addEventListener('load', function () {
 
-    function setupImageSlider(containerClass, imgClass, interval = 6000) {
-      const images = document.querySelectorAll(`.${containerClass} .${imgClass}`);
-      let number = 0;
-  
-      function changeImage() {
-        images[number].classList.remove('active');
-        number = (number + 1) % images.length;
-        images[number].classList.add('active');
-      }
-  
-      // スライドショーを開始
-      setInterval(changeImage, interval);
+  function setupImageSlider(containerClass, imgClass, interval = 6000) {
+    const images = document.querySelectorAll(`.${containerClass} .${imgClass}`);
+    let number = 0;
+
+    function changeImage() {
+      images[number].classList.remove('active');
+      number = (number + 1) % images.length;
+      images[number].classList.add('active');
     }
+
+    // スライドショーを開始
+    setInterval(changeImage, interval);
+  }
+
+  // mv1とmv2でスライドショーを初期化
+  setupImageSlider('mv1', 'mv1__img');
+  setupImageSlider('mv2', 'mv2__img');
+});
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function () {
+  const top = document.getElementById('btn__top');
+  const bottom = document.getElementById('btn__bottom');
+  const gnav = document.getElementById('gnav');
   
-    // mv1とmv2でスライドショーを初期化
-    setupImageSlider('mv1', 'mv1__img');
-    setupImageSlider('mv2', 'mv2__img');
-  });
-  
+  top.classList.toggle('btnNav');
+  bottom.classList.toggle('btnNav');
+  gnav.classList.toggle('translateNav');
+});
