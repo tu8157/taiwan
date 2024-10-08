6f1ea7e3afbfd95fa773ae4fa1fe6144ca749e3f
tu8157 <tu8157642@gmail.com>
2024-10-08 17:29:05 +0900

    10/8

--- js/function.js
+++ js/function.js
@@ -28,6 +28,33 @@ window.addEventListener('load', function () {
 });
 
 
+
+
+function bodyFix() {
+  const scrollPosi = window.scrollY;  // 現在のスクロール位置を取得
+  document.documentElement.style.position = 'fixed';
+  document.documentElement.style.width = '100%';
+  document.documentElement.style.zIndex = '1';
+  document.documentElement.style.top = `-${scrollPosi}px`;  // 負のスクロール位置を設定
+  document.body.style.position = 'fixed';
+  document.body.style.width = '100%';
+  document.body.style.zIndex = '1';
+  document.body.style.top = `-${scrollPosi}px`;
+}
+
+function bodyFixReset() {
+  const scrollPosi = parseInt(document.body.style.top || '0');  // 現在のスクロール位置を取得
+  document.documentElement.style.position = 'relative';
+  document.documentElement.style.width = 'auto';
+  document.documentElement.style.top = 'auto';
+  document.body.style.position = 'relative';
+  document.body.style.width = 'auto';
+  document.body.style.top = 'auto';
+  
+  window.scrollTo(0, -scrollPosi);  // スクロール位置を元に戻す
+}
+
+
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function () {
@@ -38,4 +65,9 @@ btn.addEventListener('click', function () {
   top.classList.toggle('btnNav');
   bottom.classList.toggle('btnNav');
   gnav.classList.toggle('translateNav');
+  if(gnav.classList.contains('translateNav') === true) {
+    bodyFix()
+  }else {
+    bodyFixReset()
+  }
 });
